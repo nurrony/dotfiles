06c06288d5e2fad68fad59737d3d4bedd40c758a
Nur Rony <pro.nmrony@gmail.com>
2016-10-30 20:08:53 +0600

    massive rewrite in setup script

--- setup.sh
+++ setup.sh
@@ -1,16 +1,36 @@
 #!/usr/bin/env bash
-sudo apt-get -y install zsh && \
-sudo chsh -s $(which zsh) && \
+printf "\npreparing your new Mac for awesomeness!!\n\n"
+which -s brew
+if [[ $? != 0 ]] ; then
+  # Install Homebrew
+  echo "Homebrew not found installing it for you..."
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" >/dev/null 2>&1
+else
+  echo 'Updating Homebrew...'
+  brew update >/dev/null 2>&1
+fi
+
+brew install zsh
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q '/usr/local/bin/zsh' /etc/shells; then
+  echo '/usr/local/bin/zsh' | sudo tee -a /etc/shells;
+  chsh -s /usr/local/bin/zsh;
+  sudo dscl . -create /Users/$USER UserShell /usr/local/bin/zsh
+fi;
+
 #Install Oh-My-ZSH
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" && \
+echo 'Installing Oh My Zsh'
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" >/dev/null 2>&1 && \
 #Install Shell plugins
+echo "installing custom plugins"
+brew install thefuck >/dev/null 2>&1
 mkdir -p ~/.oh-my-zsh/custom/plugins && cd $_ &&  \
-git clone https://github.com/djui/alias-tips.git && \
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting && \
+git clone https://github.com/djui/alias-tips.git >/dev/null 2>&1 && \
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting >/dev/null 2>&1 && \
 cd $HOME && \
-curl -SsLo hostess https://github.com/cbednarski/hostess/releases/download/v0.2.0/hostess_linux_amd64 && \
-sudo mv hostess /usr/local/bin/ && sudo chmod +x /usr/local/bin/hostess && \
-sudo -H pip3 install thefuck
+curl -SsLo hostess https://github.com/cbednarski/hostess/releases/download/v0.2.0/hostess_darwin_amd64 >/dev/null 2>&1 && \
+sudo mv hostess /usr/local/bin/ && sudo chmod +x /usr/local/bin/hostess
 mkdir $HOME/.marker && cd $HOME/.marker && \
-git clone https://github.com/pindexis/marker . && ./install.py
-rm -fr $HOME/.git
+git clone https://github.com/pindexis/marker . >/dev/null 2>&1 && ./install.py
+
+

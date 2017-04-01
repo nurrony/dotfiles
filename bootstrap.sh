354203b27333538b9bd24176dab2104a03737e25
Nur Rony <pro.nmrony@gmail.com>
2017-04-01 15:53:23 +0600

    minor updates in bootstrap and setup script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-echo "Installing Zsh and set is as default login shell" && \
+echo "Installing Zsh and set is as default login shell..." && \
 brew install zsh
 # Switch to using brew-installed bash as default shell
 if ! fgrep -q '/usr/local/bin/zsh' /etc/shells; then
@@ -9,7 +9,7 @@ if ! fgrep -q '/usr/local/bin/zsh' /etc/shells; then
 fi;
 
 #Install Oh-My-ZSH
-echo 'Installing Oh My Zsh'
+echo 'Installing Oh My Zsh...'
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" >/dev/null 2>&1 && \
 #Install Shell plugins
 echo "installing custom plugins: thefuck, alias-tips, zsh-syntax-highlighting, hostess, marker"
@@ -25,15 +25,15 @@ git clone https://github.com/pindexis/marker . >/dev/null 2>&1 && ./install.py &
 
 # clone .dotfile
 rm -fr $HOME/.dotfiles/.git && \
-mv .zshrc .zshrc.pre-nurrony-dotfiles && cp $HOME/.dotfiles/.zshrc $HOME && \
-ln -s $HOME/.dotfiles/.{gitignore,gitconfig,gitattributes,gemrc,inputrc,curlrc,gvimrc,hushlogin,wgetrc,screenrc} $HOME/
+mv .zshrc .zshrc.pre-nurrony-dotfiles && \
+ln -sf $HOME/.dotfiles/.{zshrc,curlrc,editorconfig,gemrc,gitattribute,gitconfig,gitignore,gvimrc,hgignore,hushlogin,inputrc,screenrc,.spectacles-keymap,vimrc,wgetrc} $HOME/
 echo 'All are done. Applying changes..'
-source $HOME/.zshrc
+source $HOME/.zshrc && \
 printf "\nTo prepare your pc more developer friendly you can run the following scripts\n\n"
 printf 'chmod +x ./brew.sh && ./brew.sh\n\n'
 
 printf "To customise your macOS installation according to most developer\'s preferences run the following command\n"
 
-printf 'chmod +x ./.macos.sh && ./.macos.sh\n\n'
+printf 'chmod +x ./.macos && ./.macos\n\n'
 
 echo 'Happy Hacking!!!'

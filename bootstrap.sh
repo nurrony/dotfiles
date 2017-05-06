cd8ef5279b900e254379ac594077a5a2cf963e4d
Nur Rony <pro.nmrony@gmail.com>
2017-05-06 21:51:16 +0600

    updates setup scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,20 +9,25 @@ if ! fgrep -q '/usr/local/bin/zsh' /etc/shells; then
 fi;
 
 #Install Oh-My-ZSH
-echo 'Installing Oh My Zsh...'
+echo -ne 'Installing Oh My Zsh... '
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" >/dev/null 2>&1 && \
+echo 'done' && \
+echo -ne 'Installing PowerLevel9k theme...'
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k  2>&1 && \
+echo 'done' && \
 #Install Shell plugins
-echo "installing custom plugins: thefuck, alias-tips, zsh-syntax-highlighting, hostess, marker"
+echo -ne "installing custom plugins: thefuck, alias-tips, zsh-syntax-highlighting, hostess... "
 brew install thefuck >/dev/null 2>&1
 mkdir -p ~/.oh-my-zsh/custom/plugins && cd $_ &&  \
 git clone https://github.com/djui/alias-tips.git >/dev/null 2>&1 && \
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting >/dev/null 2>&1 && \
 cd $HOME && \
 curl -SsLo hostess https://github.com/cbednarski/hostess/releases/download/v0.2.0/hostess_darwin_amd64 >/dev/null 2>&1 && \
-sudo mv hostess /usr/local/bin/ && sudo chmod +x /usr/local/bin/hostess
-mkdir $HOME/.marker && cd $HOME/.marker && \
-git clone https://github.com/pindexis/marker . >/dev/null 2>&1 && ./install.py && cd $HOME && rm -fr .marker
-
+sudo mv hostess /usr/local/bin/ && sudo chmod +x /usr/local/bin/hostess && \
+echo "done" && \
+echo -ne 'Installing NVM... '
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash 2>&1 && \
+echo 'done'
 # clone .dotfile
 rm -fr $HOME/.dotfiles/.git && \
 mv .zshrc .zshrc.pre-nurrony-dotfiles && \

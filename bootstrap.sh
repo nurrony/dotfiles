a3426fbdcdfe31b4f98ef63e00da4bd02fe88af2
Nur Rony <pro.nmrony@gmail.com>
2022-03-10 23:43:21 +0600

    remove all reference of ohmyzsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,34 +1,20 @@
 #!/usr/bin/env bash
 echo "Installing Zsh and set is as default login shell..." && \
 brew install zsh
-# Switch to using brew-installed bash as default shell
+# Switch to using brew-installed zsh as default shell
 if ! fgrep -q '/usr/local/bin/zsh' /etc/shells; then
   echo '/usr/local/bin/zsh' | sudo tee -a /etc/shells;
-  chsh -s /usr/local/bin/zsh;
+  sudo chsh -s /usr/local/bin/zsh;
   sudo dscl . -create /Users/$USER UserShell /usr/local/bin/zsh
 fi;
 
-#Install Oh-My-ZSH
-echo -ne 'Installing Oh My Zsh... '
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" >/dev/null 2>&1 && \
-echo 'done' && \
-echo -ne 'Installing PowerLevel9k theme...'
-git clone --depth=1 https://github.com/romkatv/powerlevel10k ~/.oh-my-zsh/custom/themes/powerlevel10k  2>&1 && \
-echo 'done' && \
 #Install Shell plugins
-echo -ne "installing custom plugins: thefuck, alias-tips, zsh-syntax-highlighting, hostess... "
+echo -ne "installing custom plugins: thefuck, hostess... "
 brew install thefuck hostess >/dev/null 2>&1
-mkdir -p ~/.oh-my-zsh/custom/plugins && cd $_ &&  \
-git clone https://github.com/djui/alias-tips.git >/dev/null 2>&1 && \
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting >/dev/null 2>&1 && \
 cd $HOME && \
-echo -ne 'Installing NVM... '
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash 2>&1 && \
-echo 'done'
 # clone .dotfile
-rm -fr $HOME/.dotfiles/.git && \
 mv .zshrc .zshrc.pre-nurrony-dotfiles && \
-ln -sf $HOME/.dotfiles/.{zshrc,p10k.zsh,curlrc,gemrc,cli-packages,gitattribute,gitconfig,gitignore,gvimrc,hgignore,hushlogin,inputrc,screenrc,starship,vimrc,wgetrc} $HOME/
+ln -sf $HOME/.dotfiles/.{zshrc,curlrc,gemrc,gitattribute,gitconfig,gitignore,vimrc,wgetrc} $HOME/
 echo 'All are done. Applying changes..'
 source $HOME/.zshrc && \
 printf "\nTo prepare your pc more developer friendly you can run the following scripts\n\n"

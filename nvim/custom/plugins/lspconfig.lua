5c0e4b819f9cfda49d4e27cfbe07623fa77a5b4a
Nur Rony <pro.nmrony@gmail.com>
2022-09-10 15:09:22 +0600

    adopt nvchad new plugin syntax

--- nvim/custom/plugins/lspconfig.lua
+++ nvim/custom/plugins/lspconfig.lua
@@ -1,7 +1,8 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
-local lspconfig = require("lspconfig")
+local lspconfig = require "lspconfig"
+
 local servers = {
   "html",
   "cssls",
@@ -16,8 +17,8 @@ local servers = {
 }
 
 for _, lsp in ipairs(servers) do
-	lspconfig[lsp].setup({
-		on_attach = on_attach,
-		capabilities = capabilities,
-	})
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
 end

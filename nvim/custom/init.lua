9255a592dd678495d5161f329c9761c1cd7d5d21
Nur Rony <pro.nmrony@gmail.com>
2024-01-13 00:26:54 +0600

    remove nvim to add minimal needed version

--- nvim/custom/init.lua
+++
@@ -1,8 +0,0 @@
--- commands
-
--- replace string from file
-local function sed(from, to, fname)
-  vim.cmd(string.format("silent !sed -i 's/%s/%s/g' %s", from, to, fname))
-end
-
-require "custom.commands"

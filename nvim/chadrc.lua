9b2013d9f70a9d0fd27ad351d4b9c692b28ed60d
Nur Rony <pro.nmrony@gmail.com>
2024-01-14 02:58:00 +0600

    customize nvim

--- nvim/chadrc.lua
+++ nvim/chadrc.lua
@@ -5,8 +5,7 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "onedark",
-  theme_toggle = { "onedark", "one_light" },
+  theme = "default",
 
   hl_override = highlights.override,
   hl_add = highlights.add,

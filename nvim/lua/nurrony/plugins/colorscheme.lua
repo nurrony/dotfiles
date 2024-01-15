1edf6af83c6539d06ef32582eb0f3d79a7878135
Nur Rony <pro.nmrony@gmail.com>
2024-01-16 00:29:59 +0600

    add colorscheme plugin set theme shade of purple and its configuration

---
+++ nvim/lua/nurrony/plugins/colorscheme.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    'Rigellute/shades-of-purple.vim',
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- load the colorscheme here
+      vim.cmd([[colorscheme shades_of_purple]])
+    end,
+  },
+}

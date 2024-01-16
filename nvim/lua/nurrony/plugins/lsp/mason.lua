546acd901f0c6104021c05298c242cc199b8dc6b
Nur Rony <pro.nmrony@gmail.com>
2024-01-17 00:12:14 +0600

    add mason plugin and its configuration

---
+++ nvim/lua/nurrony/plugins/lsp/mason.lua
@@ -0,0 +1,63 @@
+return {
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+  },
+  config = function()
+    -- import mason
+    local mason = require("mason")
+
+    -- import mason-lspconfig
+    local mason_lspconfig = require("mason-lspconfig")
+
+    local mason_tool_installer = require("mason-tool-installer")
+
+    -- enable mason and configure icons
+    mason.setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    })
+
+    mason_lspconfig.setup({
+      -- list of servers for mason to install
+      ensure_installed = {
+        "tsserver",
+        "emmet_ls",
+        "pyright",
+        "lua_ls",
+        "bashls",
+        "terraformls",
+        "cssls",
+        "html",
+        "jsonls",
+        "eslint",
+        "yamlls",
+        "dockerls",
+        "docker_compose_language_service"
+      },
+      -- auto-install configured servers (with lspconfig)
+      automatic_installation = true, -- not the same as ensure_installed
+    })
+
+    mason_tool_installer.setup({
+      ensure_installed = {
+        "shfmt", -- shell formatter
+        "prettier", -- prettier formatter
+        "stylua", -- lua formatter
+        "isort", -- python formatter
+        "black", -- python formatter
+        "pylint", -- python linter
+        "eslint_d", -- js linter
+        'tflint', -- terraform linter
+        'yamllint', -- yaml lister
+        "cfn-lint", -- cloudformation linter
+      },
+    })
+  end,
+}

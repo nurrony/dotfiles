2a26e2e90a3a68be1213602786a5d09ad129d747
Nur Rony <pro.nmrony@gmail.com>
2024-01-17 23:30:01 +0600

    add which key and terminal and config it

---
+++ nvim/lua/nurrony/plugins/terminal.lua
@@ -0,0 +1,29 @@
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  config = function()
+    local toggleterm = require("toggleterm")
+    toggleterm.setup({
+      size = 20,
+      open_mapping = [[<c-`>]],
+      direction = 'float',
+      hide_numbers = true, -- hide the number column in toggleterm buffers
+      shade_filetypes = {},
+      autochdir = true,
+      persist_mode = true,
+      persist_size = true,
+      close_on_exit = true,
+      shell = vim.o.shell,
+      insert_mappings = true,
+      float_opts = {
+        border = 'double',
+        winblend = 0,
+        highlights = {
+          border = 'Normal',
+          background = 'Normal',
+        },
+      },
+
+    })
+  end
+ }

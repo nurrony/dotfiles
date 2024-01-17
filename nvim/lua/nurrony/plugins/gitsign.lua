3202822df8519371f91f1124d0b21fc3926f1b10
Nur Rony <pro.nmrony@gmail.com>
2024-01-17 23:26:12 +0600

    update gitsign plugin and its configuration

--- nvim/lua/nurrony/plugins/gitsign.lua
+++ nvim/lua/nurrony/plugins/gitsign.lua
@@ -1,5 +1,14 @@
 return {
   "lewis6991/gitsigns.nvim",
   event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    signs = {
+      add = { text = '+' },
+      change = { text = '~' },
+      delete = { text = '_' },
+      topdelete = { text = '‾' },
+      changedelete = { text = '~' },
+    },
+  },
   config = true,
 }

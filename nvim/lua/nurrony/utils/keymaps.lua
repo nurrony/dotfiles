ed8abcb406a5643db9ef9464cc2aacac2aa528f4
Nur Rony <pro.nmrony@gmail.com>
2024-01-19 15:47:07 +0600

    add necessary function

--- nvim/lua/nurrony/utils/keymaps.lua
+++ nvim/lua/nurrony/utils/keymaps.lua
@@ -24,3 +24,51 @@ local get_opts = function(opts)
   end
   return all_opts
 end
+
+
+
+--- @param vimmode (string|nil)
+--- @return string
+local get_mode = function(vimmode)
+  local modeString = vim_modes[vimmode]
+  if modeString == nil then
+    return "n"
+  else
+    return modeString
+  end
+end
+
+--- @param command (string)
+--- @return string
+local get_cmd_string = function(command)
+  return [[<cmd>]] .. command .. [[<CR>]]
+end
+
+--- @param keymaps string
+--- @param command string
+--- @param vimmode (string|nil)
+--- @param options (table|nil)
+--- @return nil
+local mapvimkey = function(keymaps, command, vimmode, options)
+  local mode = get_mode(vimmode)
+  local lhs = keymaps
+  local rhs = get_cmd_string(command)
+  local opts = get_opts(options)
+  vim.keymap.set(mode, lhs, rhs, opts)
+end
+
+--- @param keymaps string
+--- @param cmd (function|string)
+--- @param desc (string|nil)
+--- @return table
+local maplazykey = function(keymaps, cmd, desc)
+  if type(cmd) ~= "function" then
+    cmd = get_cmd_string(cmd)
+  end
+  return { keymaps, cmd, desc = desc }
+end
+
+return {
+  mapvimkey = mapvimkey,
+  maplazykey = maplazykey,
+}

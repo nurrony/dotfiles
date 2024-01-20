f0d2fb336df5b7dfba1a482e97212365607b8057
Nur Rony <pro.nmrony@gmail.com>
2024-01-20 13:53:26 +0600

    update alpha setting

--- nvim/lua/nurrony/plugins/alpha.lua
+++ nvim/lua/nurrony/plugins/alpha.lua
@@ -1,36 +1,98 @@
+
 return {
-  "goolord/alpha-nvim",
-  event = "VimEnter",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+  'goolord/alpha-nvim',
+  dependencies = {
+    "nvim-tree/nvim-web-devicons"
+  },
+  enabled = true,
   config = function()
     local alpha = require("alpha")
     local dashboard = require("alpha.themes.dashboard")
+    local fortune = require("alpha.fortune")
 
-    -- Set header
-    dashboard.section.header.val = {
-      "                                                     ",
-      "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-      "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-      "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-      "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-      "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-      "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-      "                                                     ",
+    local header_collection = {}
+
+    header_collection.default = {
+      "                                                    ",
+      " ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+      " ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+      " ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+      " ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+      " ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+      " ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+      "                                                    ",
     }
 
-    -- Set menu
+    local header = header_collection.default
+
+    -- Make the header a bit more fun with some color!
+    local function colorize_header()
+      local lines = {}
+
+      for _, chars in pairs(header) do
+        local line = {
+          type = "text",
+          val = chars,
+          opts = {
+            hl = "Function",
+            shrink_margin = true,
+            position = "center",
+          },
+        }
+
+        table.insert(lines, line)
+      end
+
+      return lines
+    end
+
     dashboard.section.buttons.val = {
-      dashboard.button("e", " New File", "<cmd>ene<CR>"),
-      dashboard.button("Ctrl n", " Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-      dashboard.button("SPC ff", "󰱼 Find File", "<cmd>Telescope find_files<CR>"),
-      dashboard.button("SPC fs", " Find Word", "<cmd>Telescope live_grep<CR>"),
-      dashboard.button("q", " Quit NVIM", "<cmd>qa<CR>"),
+      dashboard.button("e", "  New file", ":ene<CR>"),
+      dashboard.button("f", "  Find file", ":Telescope find_files<CR>"),
+      dashboard.button("r", "  Find recent file", ":Telescope oldfiles<CR>"),
+      dashboard.button("g", "  Find word", ":Telescope live_grep<CR>"),
+      dashboard.button("s", "  Settings", ":Telescope find_files cwd=~/.configs/nvim/<CR>"),
+      dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
     }
 
-    -- Send config to alpha
-    alpha.setup(dashboard.opts)
+    -- Everyone could use a good fortune cookie from time to time, right?
+    dashboard.section.footer.val = fortune()
 
-    -- Disable folding on alpha buffer
-    vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
-  end,
+    -- Hide all the unnecessary visual elements while on the dashboard, and add
+    -- them back when leaving the dashboard.
+    local group = vim.api.nvim_create_augroup("CleanDashboard", {})
+
+    vim.api.nvim_create_autocmd("User", {
+      group = group,
+      pattern = "AlphaReady",
+      callback = function()
+        vim.opt.showtabline = 0
+        vim.opt.showmode = false
+        vim.opt.showcmd = false
+        vim.opt.ruler = false
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("BufUnload", {
+      group = group,
+      pattern = "<buffer>",
+      callback = function()
+        vim.opt.showtabline = 2
+        vim.opt.showmode = false
+        vim.opt.showcmd = true
+        vim.opt.ruler = true
+      end,
+    })
+
+    alpha.setup({
+      layout = {
+        { type = "padding", val = 1 },
+        { type = "group", val = colorize_header() },
+        { type = "padding", val = 2 },
+        dashboard.section.buttons,
+        dashboard.section.footer,
+      },
+      opts = { margin = 5 },
+    })
+  end
 }

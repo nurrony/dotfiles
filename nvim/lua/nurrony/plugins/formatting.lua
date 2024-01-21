0049ac1188eab1f87715c7e181e3dabcc2ce4ae2
Nur Rony <pro.nmrony@gmail.com>
2024-01-21 13:07:47 +0600

    update formating

--- nvim/lua/nurrony/plugins/formatting.lua
+++ nvim/lua/nurrony/plugins/formatting.lua
@@ -32,7 +32,7 @@ return {
       conform.format({
         lsp_fallback = true,
         async = false,
-        timeout_ms = 1000,
+        timeout_ms = 500,
       })
     end, { desc = "Format file or range (in visual mode)" })
   end,

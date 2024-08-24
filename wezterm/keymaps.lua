7cb7e5f1e42f817a02e9962c2e6fb48f9e4c0172
Nur Rony <pro.nmrony@gmail.com>
2024-08-25 01:57:05 +0600

    update wezterm

--- wezterm/keymaps.lua
+++ wezterm/keymaps.lua
@@ -1,15 +1,16 @@
 local wezterm = require("wezterm")
 local key_util = require("utils/keys")
+local action = wezterm.action;
 
 return {
   -- pane splitting
-  key_util.cmd_key("d", wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" })),
-  key_util.key_table("CMD | SHIFT", 'd', wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" })),
+  key_util.cmd_key("d", action.SplitHorizontal({ domain = "CurrentPaneDomain" })),
+  key_util.key_table("CMD | SHIFT", 'd', action.SplitVertical({ domain = "CurrentPaneDomain" })),
 
   -- close the window
-  key_util.cmd_key("w", wezterm.action.CloseCurrentPane({ confirm = false })),
+  key_util.cmd_key("w", action.CloseCurrentPane({ confirm = false })),
 
   -- Make Page up/down work
-  { key = "PageUp",   action = wezterm.action.ScrollByPage(-1) },
-  { key = "PageDown", action = wezterm.action.ScrollByPage(1) },
+  { key = "PageUp",   action = action.ScrollByPage(-1) },
+  { key = "PageDown", action = action.ScrollByPage(1) },
 }

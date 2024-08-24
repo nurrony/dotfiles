94648d071b665ea7b6d3653c675a8f914db70541
Nur Rony <pro.nmrony@gmail.com>
2024-08-25 02:35:19 +0600

    update wezterm configuration

--- wezterm/config.lua
+++ wezterm/config.lua
@@ -6,20 +6,15 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
--- The filled in variant of the < symbol
-local SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_right_hard_divider
-
--- The filled in variant of the > symbol
-local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
-
 config = {
+
   -- font
   font_size = 16,
   adjust_window_size_when_changing_font_size = false,
-  font = wezterm.font { family = "JetBrainsMono Nerd Font Mono", weight = 'Bold' },
+  font = wezterm.font { family = 'JetBrainsMono Nerd Font Mono', weight = 'Bold' },
 
   -- theme
-  color_scheme = "Catppuccin Macchiato",
+  color_scheme = 'Catppuccin Macchiato',
 
   -- tab bar
   enable_tab_bar = true,
@@ -31,10 +26,10 @@ config = {
   show_new_tab_button_in_tab_bar = false,
 
   -- window configuration
-  window_decorations = "RESIZE",
+  window_decorations = 'RESIZE',
   window_background_opacity = 0.90,
   macos_window_background_blur = 10,
-  window_close_confirmation = "NeverPrompt",
+  window_close_confirmation = 'NeverPrompt',
   window_padding = {
     left = 10,
     right = 10,
@@ -42,11 +37,11 @@ config = {
     bottom = 10,
   },
   window_frame = {
-    font = wezterm.font({ family = "JetBrainsMono Nerd Font Mono", weight = "Bold" }),
+    font = wezterm.font({ family = 'JetBrainsMono Nerd Font Mono', weight = 'Bold' }),
     font_size = 13,
     -- Fancy tab bar
-    active_titlebar_bg = "#222222",
-    inactive_titlebar_bg = "#352a21",
+    active_titlebar_bg = '#222222',
+    inactive_titlebar_bg = '#352a21',
   },
 
   -- macos related settings
@@ -58,26 +53,26 @@ config = {
   hyperlink_rules = {
     -- Matches: a URL in parens: (URL)
     {
-      regex = "\\((\\w+://\\S+)\\)",
-      format = "$1",
+      regex = '\\((\\w+://\\S+)\\)',
+      format = '$1',
       highlight = 1,
     },
     -- Matches: a URL in brackets: [URL]
     {
-      regex = "\\[(\\w+://\\S+)\\]",
-      format = "$1",
+      regex = '\\[(\\w+://\\S+)\\]',
+      format = '$1',
       highlight = 1,
     },
     -- Matches: a URL in curly braces: {URL}
     {
-      regex = "\\{(\\w+://\\S+)\\}",
-      format = "$1",
+      regex = '\\{(\\w+://\\S+)\\}',
+      format = '$1',
       highlight = 1,
     },
     -- Matches: a URL in angle brackets: <URL>
     {
-      regex = "<(\\w+://\\S+)>",
-      format = "$1",
+      regex = '<(\\w+://\\S+)>',
+      format = '$1',
       highlight = 1,
     },
     -- Then handle URLs not wrapped in brackets
@@ -86,14 +81,14 @@ config = {
       --regex = '\\b\\w+://\\S+[)/a-zA-Z0-9-]+',
       --format = '$0',
       -- After
-      regex = "[^(]\\b(\\w+://\\S+[)/a-zA-Z0-9-]+)",
-      format = "$1",
+      regex = '[^(]\\b(\\w+://\\S+[)/a-zA-Z0-9-]+)',
+      format = '$1',
       highlight = 1,
     },
     -- implicit mailto link
     {
-      regex = "\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b",
-      format = "mailto:$0",
+      regex = '\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b',
+      format = 'mailto:$0',
     },
   },
 
--- wezterm/keymaps.lua
+++ wezterm/keymaps.lua
@@ -1,22 +1,22 @@
-local wezterm = require("wezterm")
-local key_util = require("utils/keys")
+local wezterm = require('wezterm')
+local key_util = require('utils/keys')
 local action = wezterm.action;
 
 return {
   -- panes: splitting
-  key_util.cmd_key("d", action.SplitHorizontal({ domain = "CurrentPaneDomain" })),
-  key_util.key_table("CMD | SHIFT", 'd', action.SplitVertical({ domain = "CurrentPaneDomain" })),
+  key_util.cmd_key('d', action.SplitHorizontal({ domain = 'CurrentPaneDomain' })),
+  key_util.key_table('CMD | SHIFT', 'd', action.SplitVertical({ domain = 'CurrentPaneDomain' })),
 
   -- panes: selection
-  key_util.cmd_key("k", action.ActivatePaneDirection("Up")),
-  key_util.cmd_key("j", action.ActivatePaneDirection("Down")),
-  key_util.cmd_key("h", action.ActivatePaneDirection("Left")),
-  key_util.cmd_key("l", action.ActivatePaneDirection("Right")),
+  key_util.cmd_key('k', action.ActivatePaneDirection('Up')),
+  key_util.cmd_key('j', action.ActivatePaneDirection('Down')),
+  key_util.cmd_key('h', action.ActivatePaneDirection('Left')),
+  key_util.cmd_key('l', action.ActivatePaneDirection('Right')),
 
   -- window: close the window
-  key_util.cmd_key("w", action.CloseCurrentPane({ confirm = false })),
+  key_util.cmd_key('w', action.CloseCurrentPane({ confirm = false })),
 
   -- Make Page up/down work
-  key_util.key_table(nil, "PageUp", action.ScrollByPage(-1)),
-  key_util.key_table(nil, "PageDown", action.ScrollByPage(1)),
+  key_util.key_table(nil, 'PageUp', action.ScrollByPage(-1)),
+  key_util.key_table(nil, 'PageDown', action.ScrollByPage(1)),
 }

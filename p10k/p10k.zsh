6d0a403e247d9bb6c800bd87298169a43d0bf20c
Nur Rony <pro.nmrony@gmail.com>
2024-10-15 17:54:41 +0600

    update p10k for mise

--- p10k/p10k.zsh
+++ p10k/p10k.zsh
@@ -47,7 +47,8 @@
   typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
     # =========================[ Line #1 ]=========================
     # direnv                  # direnv status (https://direnv.net/)
-    asdf                      # asdf version manager (https://github.com/asdf-vm/asdf)
+    # asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
+    mise                      # mise version manager (https://github.com/jdx/mise)
     virtualenv                # python virtual environment (https://docs.python.org/3/library/venv.html)
     anaconda                  # conda environment (https://conda.io/)
     pyenv                     # python environment (https://github.com/pyenv/pyenv)
@@ -1814,6 +1815,76 @@
   typeset -g POWERLEVEL9K_EXAMPLE_FOREGROUND=3
   typeset -g POWERLEVEL9K_EXAMPLE_BACKGROUND=1
   # typeset -g POWERLEVEL9K_EXAMPLE_VISUAL_IDENTIFIER_EXPANSION='⭐'
+
+ # MISE
+function prompt_mise() {
+    local plugins=("${(@f)$(mise ls --current 2>/dev/null | awk '!/\(symlink\)/ && $3!="~/.tool-versions" && $3!="~/.config/mise/config.toml" {print $1, $2}')}")
+    local plugin
+    for plugin in ${(k)plugins}; do
+      local parts=("${(@s/ /)plugin}")
+      local tool=${(U)parts[1]}
+      local version=${parts[2]}
+      p10k segment -r -i "${tool}_ICON" -s $tool -t "$version"
+    done
+  }
+
+  # icons
+  typeset -g POWERLEVEL9K_PYTHON_ICON=''
+  typeset -g POWERLEVEL9K_ELIXIR_ICON=''
+  typeset -g POWERLEVEL9K_ERLANG_ICON=''
+  typeset -g POWERLEVEL9K_MISE_LUA_ICON=''
+  typeset -g POWERLEVEL9K_MISE_PHP_ICON=''
+  typeset -g POWERLEVEL9K_MISE_DENO_ICON='󰯵'
+  typeset -g POWERLEVEL9K_MISE_NODE_ICON=''
+  typeset -g POWERLEVEL9K_MISE_PERL_ICON=''
+  typeset -g POWERLEVEL9K_MISE_JAVA_ICON=''
+  typeset -g POWERLEVEL9K_MISE_RUST_ICON=''
+  typeset -g POWERLEVEL9K_MISE_DART_ICON=''
+  typeset -g POWERLEVEL9K_MISE_RUBY_ICON=''
+  typeset -g POWERLEVEL9K_MISE_MAVEN_ICON=''
+  typeset -g POWERLEVEL9K_MISE_JULIA_ICON=''
+  typeset -g POWERLEVEL9K_MISE_SCALA_ICON=''
+  typeset -g POWERLEVEL9K_MISE_PYTHON_ICON=''
+  typeset -g POWERLEVEL9K_MISE_GRADLE_ICON=''
+  typeset -g POWERLEVEL9K_MISE_PACKAGE_ICON='󰏗'
+  typeset -g POWERLEVEL9K_MISE_HASKELL_ICON=''
+  typeset -g POWERLEVEL9K_MISE_DOTNET_CORE_ICON=''
+
+# Colors
+  typeset -g POWERLEVEL9K_MISE_FOREGROUND=#43302E
+  typeset -g POWERLEVEL9K_MISE_BACKGROUND=#95ffa5
+  typeset -g POWERLEVEL9K_MISE_GRADLE_FOREGROUND=#212121
+
+  ## background
+  typeset -g POWERLEVEL9K_MISE_LUA_BACKGROUND=4
+  typeset -g POWERLEVEL9K_MISE_PHP_BACKGROUND=5
+  typeset -g POWERLEVEL9K_MISE_RUBY_BACKGROUND=1
+  typeset -g POWERLEVEL9K_MISE_PERL_BACKGROUND=4
+  typeset -g POWERLEVEL9K_MISE_JULIA_BACKGROUND=2
+  typeset -g POWERLEVEL9K_MISE_ELIXIR_BACKGROUND=5
+  typeset -g POWERLEVEL9K_MISE_ERLANG_BACKGROUND=1
+  typeset -g POWERLEVEL9K_MISE_RUST_BACKGROUND=208
+  typeset -g POWERLEVEL9K_MISE_DENO_BACKGROUND=222
+  typeset -g POWERLEVEL9K_MISE_JAVA_BACKGROUND=213
+  typeset -g POWERLEVEL9K_MISE_HASKELL_BACKGROUND=3
+  typeset -g POWERLEVEL9K_MISE_FLUTTER_BACKGROUND=4
+  typeset -g POWERLEVEL9K_MISE_POSTGRES_BACKGROUND=6
+  typeset -g POWERLEVEL9K_MISE_PYTHON_BACKGROUND=121
+  typeset -g POWERLEVEL9K_MISE_GRADLE_BACKGROUND=150
+  typeset -g POWERLEVEL9K_MISE_MAVEN_BACKGROUND=#88307b
+  typeset -g POWERLEVEL9K_MISE_DOTNET_CORE_BACKGROUND=5
+
+  # foreground
+  typeset -g POWERLEVEL9K_MISE_DENO_FOREGROUND=#212121
+  typeset -g POWERLEVEL9K_MISE_JAVA_FOREGROUND=#212121
+  typeset -g POWERLEVEL9K_MISE_MAVEN_FOREGROUND=#CCCCCC
+
+
+  typeset -g POWERLEVEL9K_MISE_GRADLE_SHOW_ON_UPGLOB='*.gradle'
+  typeset -g POWERLEVEL9K_MISE_DENO_SHOW_ON_UPGLOB='deno.json|deno.jsonc'
+  typeset -g POWERLEVEL9K_MISE_MAVEN_SHOW_ON_UPGLOB='pom.xml|mvnw|mvnw.cmd'
+  typeset -g POWERLEVEL9K_MISE_NODE_SHOW_ON_UPGLOB='*.js|*.ts|package.json|.nvmrc|pnpm-workspace.yaml|.pnpmfile.cjs|.vue|yarn.lock'
+  typeset -g POWERLEVEL9K_MISE_JAVA_SHOW_ON_UPGLOB='*.java|*.class|*.gradle|*.jar|*.clj|*.cljr|pom.xml|build.gradle.kts|build.sbt|.java-version|*.deps.edn|project.clj|build.boot'
 
   # Transient prompt works similarly to the builtin transient_rprompt option. It trims down prompt
   # when accepting a command line. Supported values:

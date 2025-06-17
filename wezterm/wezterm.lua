df452e7a2a1aa28fceafb02d7198fd2389e2fb14
Nur Rony <pro.nmrony@gmail.com>
2025-06-17 12:19:07 +0600

    update wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -13,6 +13,6 @@ local config = require 'config'
 local wezterm = require 'wezterm'
 local tabbar_config = require 'tabbar'
 
-wezterm.plugin.require("https://github.com/nekowinston/wezterm-bar").apply_to_config(config, tabbar_config)
+wezterm.plugin.require("https://code.winston.sh/winston/wezterm-bar").apply_to_config(config, tabbar_config)
 
 return config;
